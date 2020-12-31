a1ceeeb300f3ee4dfd73ac4a94a0abd699c3c83e
ercancotenoglu <ercancotenoglu@gmail.com>
2020-12-31 22:25:09 +0000

    bootcamp day2 completed

---
+++ src/bootCamp_Day02/ShapesTask/Shape.java
@@ -0,0 +1,31 @@
+package bootCamp_Day02.ShapesTask;
+
+public  abstract class Shape {
+
+    public String name;
+
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public double area;
+    public double perimeter;
+
+    public abstract double calculateArea();
+
+    public abstract double calculatePerimeter();
+
+
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                ", area=" + area +
+                ", perimeter=" + perimeter +
+                '}';
+    }
+
+}
+
+
